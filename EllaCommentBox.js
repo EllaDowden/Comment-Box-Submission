aa55f5671a8ecce3b370856368b6b2af2ac5f12a
EllaDowden <107041445+EllaDowden@users.noreply.github.com>
2022-11-13 13:15:42 +0000

    Add files via upload

---
+++ EllaCommentBox.js
@@ -0,0 +1,53 @@
+function commentBox() {
+  let data = "";
+  let name = document.getElementById("userName").value;
+  let email = document.getElementById("userEmail").value;
+  let comment = document.getElementById("userComment").value;
+  let count = (textContent = textarea.value.length);
+
+  data =
+    "<h5>User name: </h5>" +
+    name +
+    "<h5>User email: </h5>" +
+    email +
+    "<h5>Comment: </h5>" +
+    comment +
+    "<h5>Comment characters: </h5>" +
+    count +
+    " characters";
+
+  document.getElementById("data").innerHTML = data;
+}
+
+const max = 140;
+let textarea = document.querySelector("textarea");
+let info = document.querySelector("#info");
+
+let counter = (info.textContent = max - textarea.value.length);
+
+textarea.addEventListener("input", function () {
+  info.textContent = max - this.value.length;
+});
+
+textarea.addEventListener("input", warningAlert);
+
+function warningAlert() {
+  let warning;
+  let count = document.getElementById("userComment").value.length;
+
+  if (count > max) {
+    (warning = "Please don't exceed the 140 character limit please!"),
+      (document.getElementById("userComment").style.color = "red");
+    document.getElementById("userComment").style.border = "2px solid red";
+  } else {
+    warning = "";
+    document.getElementById("userComment").style.color = "black";
+    document.getElementById("userComment").style.border = "1px solid black";
+  }
+  document.getElementById("warningAlert").style.color = "red";
+  document.getElementById("warningAlert").innerHTML = warning;
+
+  console.log(warning);
+  console.log(count);
+  console.log(info);
+}
